8d5fb871858b6131c6419359a4af4058c1c2860a
kyooni <sylenien@gmail.com>
2019-07-01 23:54:48 +0300

    add more things

--- Pages/AllPosts/components/CardArticle.js
+++ Pages/AllPosts/components/CardArticle.js
@@ -16,26 +16,10 @@ import { setData } from '../../../Redux/articleReducer'
 import CachedImage from '../../../components/CachedImage'
 
 class CardArticle extends React.PureComponent {
-constructor(props) {
+  constructor(props) {
     super(props)
-    this.state = {
-    imgUrl: null,
-    isFetching: false,
-    error: null,
-    }
   }
 
-  componentDidMount() {
-    if (this.props.mediaUrl) {
-    fetch(this.props.mediaUrl)
-      .then(response => response.json())
-      .then(result => this.setState({ imgUrl: result && result.source_url, isFetching: false }))
-      .catch(e => {
-        console.error(e)
-        this.setState({isFetching: false, error: e })
-      })
-    }
-  }
   onItemPress = (id) => {
     const { 
       setPost, 
@@ -58,23 +42,20 @@ constructor(props) {
   }
 
   render () {
-    const { title, descr, categories, id } = this.props
-    const { imgUrl } = this.state
+    const { title, descr, categories, id, mediaUrl } = this.props
+
     return (
     <TouchableOpacity onPress={() => this.onItemPress(id)}>
       <View style={styles.card}>
 
-      {imgUrl && (
+      {mediaUrl && (
         <CachedImage
-          source={imgUrl}
+          source={mediaUrl}
           title={id}
+          categories={categories[0] ? categories[0] : undefined}
           style={{flex: 1, height: 190, borderBottomWidth: 1, borderColor: '#000'}}
         />
       )}
-      {imgUrl && categories[0] && 
-        <View style={styles.category}>
-          <Text style={{color: '#fff'}}>{categories[0].name}</Text>
-        </View>}
       <View style={styles.cardText} >
         <Text style={{fontWeight: 'bold', fontSize: 18, paddingBottom: 4}}>{title}</Text>
         <Text style={{fontSize: 14}}>{descr.split('\n<')[0]}...</Text>
@@ -104,16 +85,6 @@ const styles = StyleSheet.create({
     fontSize: 18,
     // marginTop: -14,
   },
-  category: {
-    padding: 7,
-    marginLeft: 10,
-    fontSize: 12,
-    backgroundColor: '#000',
-    borderRadius: 2,
-    color: '#fff',
-    alignSelf: 'flex-start',
-    top: -180,
-  }
 })
 
 const mapDispatchToProps = (dispatch) => ({
--- components/CachedImage.js
+++ components/CachedImage.js
@@ -1,5 +1,13 @@
 import React, { PureComponent } from 'react'
-import { View, Text, Image, ActivityIndicator, Dimensions, Platform } from 'react-native'
+import { 
+  StyleSheet,
+  View,
+  Text, 
+  Image, 
+  ActivityIndicator, 
+  Dimensions, 
+  Platform 
+} from 'react-native'
 import { FileSystem } from 'expo'
 
 class CachedImage extends PureComponent {
@@ -10,29 +18,34 @@ class CachedImage extends PureComponent {
   }
 
   componentDidMount() {
-    const extension = this.props.source.slice((this.props.source.lastIndexOf(".") - 1 >>> 0) + 2)
-    // if not jpg/png/gif => error
-    if ((extension.toLowerCase() !== 'jpg') && (extension.toLowerCase() !== 'png') && (extension.toLowerCase() !== 'gif')) {
-      this.setState({ loading: false, failed: true })
-    }
-
-    FileSystem.getInfoAsync(
-      `${FileSystem.cacheDirectory + this.props.title}.${extension}`
-    ).then(({ exists, uri }) => {
-      if (exists) this.loadLocal(uri)
-      else {
-        FileSystem.downloadAsync(
-          this.props.source,
-        `${FileSystem.cacheDirectory + this.props.title}.${extension}`
-        ).then(({ uri }) => {
-          this.loadLocal(Platform.OS === 'ios'? uri : this.props.source)
-        }).catch(e => {
-          console.log('Image loading error:', e)
-          // if the online download fails, load the local version
-          this.loadLocal(`${FileSystem.cacheDirectory + this.props.title}.${extension}`)
-        });
-      }
-    })
+    fetch(this.props.source)
+      .then(response => response.json())
+      .then(result => {
+        const url = result.source_url || '.err'
+        const extension = url.slice((url.lastIndexOf(".") - 1 >>> 0) + 2)
+        // if not jpg/png/gif => error
+        if ((extension.toLowerCase() !== 'jpg') && (extension.toLowerCase() !== 'png') && (extension.toLowerCase() !== 'gif')) {
+          this.setState({ loading: false, failed: true })
+        } else {
+          FileSystem.getInfoAsync(
+            `${FileSystem.cacheDirectory + this.props.title}.${extension}`
+          ).then(({ exists, uri }) => {
+            if (exists) this.loadLocal(uri)
+            else {
+              FileSystem.downloadAsync(
+                url,
+              `${FileSystem.cacheDirectory + this.props.title}.${extension}`
+              ).then(({ uri }) => {
+                this.loadLocal(Platform.OS === 'ios'? uri : url)
+              }).catch(e => {
+                console.log('Image loading error:', e)
+                // if the online download fails, load the local version
+                this.loadLocal(`${FileSystem.cacheDirectory + this.props.title}.${extension}`)
+              });
+            }
+          })
+        }
+      })
   }
 
   loadLocal(uri) {
@@ -40,11 +53,11 @@ class CachedImage extends PureComponent {
   }
 
   render() {
-    const { style } = this.props
+    const { style, categories } = this.props
 
     if (this.state.loading) {
       return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', ...style }}>
           <ActivityIndicator
             color='#42C2F3'
             size='large'
@@ -52,18 +65,39 @@ class CachedImage extends PureComponent {
         </View>
       )
     }
+
     if (this.state.failed) {
-      return( <Text>error</Text> );
+      return <Text></Text>
     }
+
     return (
       <View>
         <Image
           style={style}
           source={{ uri: this.state.imguri }}
+          resizeMethod="scale"
         />
+        {categories && (
+          <View style={styles.category}>
+            <Text style={{color: '#fff'}}>{categories.name}</Text>
+          </View>
+        )}
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  category: {
+    padding: 7,
+    marginLeft: 10,
+    fontSize: 12,
+    backgroundColor: '#000',
+    borderRadius: 2,
+    color: '#fff',
+    alignSelf: 'flex-start',
+    top: -180,
+  }
+})
 
 export default CachedImage
