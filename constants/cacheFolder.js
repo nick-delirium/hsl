dd07fe06df96910d284482b3dfe04f72c3fc05a6
Natalia <ginushk1@gmail.com>
2020-01-26 22:48:48 +0300

    Fix cafe folder path

--- constants/cacheFolder.js
+++ constants/cacheFolder.js
@@ -1,6 +1,6 @@
 import * as FileSystem from 'expo-file-system'
 
-const cacheFolder = `${FileSystem.cacheDirectory}/img/`
+const cacheFolder = `${FileSystem.cacheDirectory}img/`
 const cacheDateStorageKey = 'cachedate'
 
 export {
